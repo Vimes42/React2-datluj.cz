be0a44132d7c0079216a099e9c792f69bf1bdf9d
Lenka Polcerova <lenka.polcerova@gmail.com>
2025-11-15 23:44:28 +0100

    úkol č. 5 hotov

--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -22,6 +22,7 @@ const generateWord = (size: number): string | null => {
 
 const Stage = () => {
   const [words, setWords] = useState<string[]>(['jahoda', 'jablko', 'hruška']);
+  const [mistakes, setMistakes] = useState<number>(0);
   
   const handleFinish = () => {
         const oldArray = [...words];
@@ -30,16 +31,67 @@ const Stage = () => {
         if (newArray) { setWords(newArray)} //unmount komponenty Wordbox, vygenerování nového slova
       }
   
-  
+  const handleMistakes = () => {
+      setMistakes(x => x + 1);
+    }
 
   return (
     <div className="stage">
-      <div className="stage__mistakes">Chyb: 0</div>
+      <div className="stage__mistakes">Chyb: {mistakes}</div>
       <div className="stage__words">
-        {words.map((word, index) =>  <Wordbox word={word} key={word} onFinish={handleFinish} active={index === 0} /> )}
+        {words.map((word, index) =>  <Wordbox word={word} key={word} onFinish={handleFinish} active={index === 0} onMistake={handleMistakes}/> )}
       </div>
     </div>
   );
 };
 
 export default Stage;
+
+/* import { useState } from 'react';
+import Wordbox from '../Wordbox';
+import wordList from '../../word-list';
+import './style.css';
+
+
+
+
+const generateWord = (size: number): string | null => {
+  const sizeIndex = size === undefined
+    ? Math.floor(Math.random() * wordList.length)
+    : size - 3;
+  
+  if (sizeIndex < 0 || sizeIndex >= wordList.length) {
+    return null;
+  }
+  
+  const words = wordList[sizeIndex];
+  const wordIndex = Math.floor(Math.random() * words.length);
+  return words[wordIndex];
+};
+
+const Stage = () => {
+  const [words, setWords] = useState<string | null[]>(['jahoda', 'jablko', 'hruška']);
+  const [mistakes, setMistakes] = useState<number>(0)
+  
+  const handleFinish = () => {
+        const oldArray = [...words];
+        const newArray: (string | null)[] = [...oldArray.splice(1), generateWord(6)]
+        if (newArray) { setWords(newArray)} //unmount komponenty Wordbox, vygenerování nového slova
+      }
+  
+  const handleMistakes = () => {
+    setMistakes(x => x + 1)
+  }
+
+  return (
+    <div className="stage">
+      <div className="stage__mistakes">Chyb: {mistakes}</div>
+      <div className="stage__words">
+        {words.map((word, index) =>  <Wordbox word={word} key={index} onFinish={handleFinish} active={index === 0} onMistake={handleMistakes} /> )}
+      </div>
+    </div>
+  );
+};
+
+export default Stage;
+ */
--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -5,28 +5,38 @@ interface IWordboxProp {
   word: string;
   onFinish: () => void;
   active: boolean;
+  onMistake: () => void;
 }
 
-const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active }) => {
+const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake }) => {
   const [lettersLeft, setLettersLeft] = useState<string>(word); 
-  const [mistake, setMistake] = useState<boolean>(false);
+  const [mistake, setMistake] = useState<boolean>(false);  
   
+
   useEffect(
     () => {
       const handleKeyUp = (e: KeyboardEvent) => {
           setLettersLeft(
             x => {
-              if ( x.length !== 1 ){
-                setMistake(() => {
+              const isError = e.key !== x[0] && x.length > 0;
+          
+              setMistake(() => {
                   if (e.key !== x[0]) 
-                    {return true} 
-                    return false 
-                });
+                      {return true} 
+                      return false 
+              });
+                
+              if(isError) {
+                  setTimeout(() => {
+                    onMistake(); 
+                  }, 0);
+                };
+
                 return e.key === x[0] ? x.slice(1) : x;
               
-              } onFinish(); 
-            });
-      }
+              } 
+            );
+      };
        
       if (active) {
             document.addEventListener("keyup", handleKeyUp);
@@ -36,7 +46,79 @@ const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active }) => {
             }
       }
       
-    }, [lettersLeft, active]
+    }, [active]
+  )
+     /* useEffect(() => {
+      onMistake(mistakeCount)
+    }, [mistakeCount]); */
+
+    useEffect(() => {
+        if (lettersLeft.length === 0 && word.length > 0) {
+            onFinish();
+        }
+    }, [lettersLeft, word, onFinish]); // Spustí se, když se lettersLeft změní na prázdný řetězec
+
+    
+
+  return (
+    <div className={ mistake ? "wordbox wordbox--mistake" : "wordbox"}>{lettersLeft}</div>
+  );
+};
+
+
+
+export default Wordbox;
+
+/* import React, { useState, useEffect } from 'react';
+import './style.css';
+
+interface IWordboxProp {
+  word: string | null;
+  onFinish: () => void;
+  active: boolean;
+  onMistake: ()=> void;
+}
+
+const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake }) => {
+  const [lettersLeft, setLettersLeft] = useState<(string | null)>(word); 
+  const [mistake, setMistake] = useState<boolean>(false);
+  
+  useEffect(
+    () => {
+      const handleKeyUp = (e: KeyboardEvent) => {
+        if (!active) return;
+
+          setLettersLeft(x => {
+                const correct = e.key === x![0]; //true, false
+                setMistake(!correct); //nastavení zbarvení do červena, pokud není stisknuta správná klávesa
+                
+                if (!correct) {
+                  onMistake(); // spustí počítání chyby, jen když je chyba
+                  console.log("onMistake called from", word);
+                  return x; //lettersleft zatím zůstává stejné
+                }
+                const next = x!.slice(1); // odebrání prvního písmena
+
+                if (next.length === 0) {
+                  onFinish();
+                }
+
+                console.log("keydown handler in", word, "key=", e.key);
+                return next;
+              });
+      }
+       
+      if (active) {
+            document.addEventListener("keyup", handleKeyUp);
+            console.log("mount listener for", word);
+
+            return () => {
+              document.removeEventListener("keyup", handleKeyUp)
+              console.log("unmount listener for", word);
+            }
+      }
+      
+    }, [active, word]
   )
 
 
@@ -47,3 +129,4 @@ const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active }) => {
 
 export default Wordbox;
 
+ */
