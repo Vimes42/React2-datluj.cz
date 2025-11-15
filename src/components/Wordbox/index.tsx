c036439bd8d4944adef36ef7b9705277259357ce
Lenka Polcerova <lenka.polcerova@gmail.com>
2025-11-15 23:53:06 +0100

    drobné úpravy

--- src/components/Wordbox/index.tsx
+++ src/components/Wordbox/index.tsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import './style.css';
 
 interface IWordboxProp {
-  word: string;
+  word: string | null;
   onFinish: () => void;
   active: boolean;
   onMistake: () => void;
@@ -48,15 +48,13 @@ const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake })
       
     }, [active]
   )
-     /* useEffect(() => {
-      onMistake(mistakeCount)
-    }, [mistakeCount]); */
+   
 
     useEffect(() => {
         if (lettersLeft.length === 0 && word.length > 0) {
             onFinish();
         }
-    }, [lettersLeft, word, onFinish]); // Spustí se, když se lettersLeft změní na prázdný řetězec
+    }, [lettersLeft, word, onFinish]); 
 
     
 
@@ -69,64 +67,3 @@ const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake })
 
 export default Wordbox;
 
-/* import React, { useState, useEffect } from 'react';
-import './style.css';
-
-interface IWordboxProp {
-  word: string | null;
-  onFinish: () => void;
-  active: boolean;
-  onMistake: ()=> void;
-}
-
-const Wordbox : React.FC<IWordboxProp> = ({ word, onFinish, active, onMistake }) => {
-  const [lettersLeft, setLettersLeft] = useState<(string | null)>(word); 
-  const [mistake, setMistake] = useState<boolean>(false);
-  
-  useEffect(
-    () => {
-      const handleKeyUp = (e: KeyboardEvent) => {
-        if (!active) return;
-
-          setLettersLeft(x => {
-                const correct = e.key === x![0]; //true, false
-                setMistake(!correct); //nastavení zbarvení do červena, pokud není stisknuta správná klávesa
-                
-                if (!correct) {
-                  onMistake(); // spustí počítání chyby, jen když je chyba
-                  console.log("onMistake called from", word);
-                  return x; //lettersleft zatím zůstává stejné
-                }
-                const next = x!.slice(1); // odebrání prvního písmena
-
-                if (next.length === 0) {
-                  onFinish();
-                }
-
-                console.log("keydown handler in", word, "key=", e.key);
-                return next;
-              });
-      }
-       
-      if (active) {
-            document.addEventListener("keyup", handleKeyUp);
-            console.log("mount listener for", word);
-
-            return () => {
-              document.removeEventListener("keyup", handleKeyUp)
-              console.log("unmount listener for", word);
-            }
-      }
-      
-    }, [active, word]
-  )
-
-
-  return (
-    <div className={ mistake ? "wordbox wordbox--mistake" : "wordbox"}>{lettersLeft}</div>
-  );
-};
-
-export default Wordbox;
-
- */
